8f5f8fe22fa7c739f852954dd396267bbea0fe18
casjay <git-admin@casjaysdev.com>
2020-09-01 01:03:30 -0400

    Update compton.sh

--- bin/compton.sh
+++ bin/compton.sh
@@ -5,9 +5,8 @@
 #The icon that would change color
 icon=""
 
-if pgrep -x "compton" > /dev/null
-then
-	echo "%{F#00AF02}$icon " #Green
+if pgrep -x "compton" > /dev/null || pgrep -x "picom" > /dev/null; then
+    echo "%{F#00AF02}$icon " #Green
 else
-	echo "%{F#65737E}$icon " #Gray
+    echo "%{F#65737E}$icon " #Gray
 fi
