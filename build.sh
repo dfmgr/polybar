134716623869901bb88f5ea2a9d5ca8d52b6eefc
Jason <git-admin@casjaysdev.com>
2022-07-04 16:43:48 -0400

    🗃️  modified: build.sh 🗃️

--- build.sh
+++ build.sh
@@ -1,122 +1,235 @@
 #!/usr/bin/env bash
-
-SCRIPTNAME="$(basename $0)"
-SCRIPTDIR="$(dirname "${BASH_SOURCE[0]}")"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202108010012-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
+# @License       : LICENSE.md
+# @ReadME        : build.sh --help
+# @Copyright     : Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @Created       : Monday, Jul 04, 2022 16:26 EDT
+# @File          : build.sh
+# @Description   : Installer script for polybar
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$0")"
+VERSION="202108010012-git"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set bash options
+if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
+trap 'exitCode=${exitCode:-$?};[ -n "$BUILD_SH_TEMP_FILE" ] && [ -f "$BUILD_SH_TEMP_FILE" ] && rm -Rf "$BUILD_SH_TEMP_FILE" &>/dev/null;exit ${exitCode:-$?}' EXIT
+set -Eo pipefail
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup build function
+__make_build() {
+  local exitCode=1
+  local exitCode_make=0
+  local exitCode_cmake=0
+  local exitCode_configure=0
+  if [[ -f "configure" ]]; then
+    printf_color "$GREEN" "running configure"
+    ./configure \
+      --prefix="$BUILD_DESTDIR" 2>&1 |
+      tee -a "$BUILD_LOG_FILE" &>/dev/null
+    exitCode_configure=$?
+  fi
+  if [[ -f "Makefile" ]]; then
+    printf_color "$GREEN" "running make"
+    make clean 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      make -j$(nproc) 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      sudo make install DESTDIR="$BUILD_DESTDIR" 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    exitCode_make=$?
+  fi
+  if [[ -f "CMakeLists.txt" ]]; then
+    mkdir build && cd build || exit 10
+    cmake .. 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      make -j$(nproc) 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      sudo make install 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null &&
+      exitCode_cmake=$?
+  fi
+  [[ $exitCode_configure = 0 ]] &&
+    [[ $exitCode_make = 0 ]] &&
+    [[ $exitCode_cmake = 0 ]] &&
+    exitCode=0
+  return "${exitCode:-$?}"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'
+# Set main functions
+__run_git() {
+  if [[ -d ".git" ]]; then
+    printf_color "$CYAN" "Updating the git repo"
+    git reset --hard && git pull 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    if [[ $? = 0 ]]; then
+      return 0
+    else
+      printf_color "$RED" "Failed to clone from $BUILD_SRC_URL"
+      exit 1
+    fi
+  elif [[ -n "$BUILD_SRC_URL" ]]; then
+    printf_color "$CYAN" "Cloning the git repo"
+    git clone "$BUILD_SRC_URL" "$BUILD_SRC_DIR" 2>&1 | tee -a "$BUILD_LOG_FILE" &>/dev/null
+    if [[ $? = 0 ]]; then
+      return 0
+    else
+      printf_color "$RED" "Failed to clone from $BUILD_SRC_URL"
+      exit 1
+    fi
+  fi
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__check_log() {
+  local exitCode="$?"
+  if [[ -f "$BUILD_LOG_FILE" ]]; then
+    errors="$(grep 'fatal error' "$BUILD_LOG_FILE" || echo '')"
+    if [[ -n "$errors" ]] || [[ "$exitCode" -ne 0 ]]; then
+      printf_color "$RED" "The following errors have occurred:"
+      echo -e "$errors"
+      printf_color "$YELLOW" "Log file saved to $BUILD_LOG_FILE"
+      exitCode=1
+    else
+      rm -Rf "$BUILD_LOG_FILE" &>/dev/null
+      printf_color "$GREEN" "Build of $BUILD_NAME has completed without error"
+      exitCode=0
+    fi
+  fi
+  return "${exitCode:-$?}"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+printf_color() { echo -e "\t\t${1:-}${2:-}${NC}"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__help() {
+  [[ -z "$ARRAY" ]] || local array="[${ARRAY//,/ }]"
+  [[ -z "$LONGOPTS" ]] || local opts="[--${LONGOPTS//,/ --}]"
+  printf_color "$BLUE" "Usage: $APPNAME $opts $array"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Check for needed applications
+type -P bash &>/dev/null || { printf_color "$RED" "Missing: bash" && exit 1; }
+type -P make &>/dev/null || { printf_color "$RED" "Missing: make" && exit 1; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set variables
+exitCode=10
+NC="$(tput sgr0 2>/dev/null)"
+RESET="$(tput sgr0 2>/dev/null)"
+BLACK="\033[0;30m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+PURPLE="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[0;37m"
+ORANGE="\033[0;33m"
+LIGHTRED='\033[1;31m'
+BG_GREEN="\[$(tput setab 2 2>/dev/null)\]"
+BG_RED="\[$(tput setab 9 2>/dev/null)\]"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Application Folders
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# @Author      : Jason
-# @Contact     : casjaysdev@casjay.net
-# @File        : build
-# @Created     : Mon, Dec 31, 2019, 00:00 EST
-# @License     : WTFPL
-# @Copyright   : Copyright (c) CasjaysDev
-# @Description : polybar with jgmenu build script
-#
+# Show warn message if variables are missing
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Set functions
-
-if [ -f /usr/local/share/CasjaysDev/scripts/functions/app-installer.bash ]; then
-  . /usr/local/share/CasjaysDev/scripts/functions/app-installer.bash
-elif [ -f "$HOME/.local/share/scripts/functions/app-installer.bash" ]; then
-  . "$HOME/.local/share/scripts/functions/app-installer.bash"
+# Set options
+SETARGS="$*"
+SHORTOPTS=""
+LONGOPTS="version,help,raw"
+ARRAY=""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup application options
+setopts=$(getopt -o "$SHORTOPTS" --long "$LONGOPTS" -a -n "$APPNAME" -- "$@" 2>/dev/null)
+eval set -- "${setopts[@]}" 2>/dev/null
+while :; do
+  case $1 in
+  --help)
+    shift 1
+    __help
+    exit
+    ;;
+  --version)
+    shift 1
+    printf_color "$YELLOW" "$APPNAME Version: $VERSION\n"
+    exit
+    ;;
+  --raw)
+    shift 1
+    printf_color() { shift 1 && echo -e "$1"; }
+    ;;
+  --)
+    shift 1
+    ARGS="$1"
+    set --
+    break
+    ;;
+  esac
+done
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables
+BUILD_NAME="polybar"
+BUILD_SRC_URL="${BUILD_SRC_URL:-https://github.com/polybar/polybar}"
+BUILD_SRC_DIR="${BUILD_SRC_DIR:-$HOME/.local/share/$BUILD_NAME/source}"
+BUILD_LOG_FILE="${BUILD_LOG_FILE:-/tmp/$BUILD_NAME_build.log}"
+if command -v "$BUILD_NAME" | grep -q '^/bin' || command -v "$BUILD_NAME" | grep -q '^/usr/bin'; then
+  BUILD_DESTDIR="/usr"
 else
-  mkdir -p "$HOME/.local/share/scripts/functions"
-  curl -LSs https://github.com/dfmgr/installer/raw/main/functions/app-installer.bash -o "$HOME/.local/share/scripts/functions/app-installer.bash"
-  . "$HOME/.local/share/scripts/functions/app-installer.bash"
+  BUILD_DESTDIR="${BUILD_DESTDIR:-/usr/local}"
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-sudoask
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# requires
+# Install required packages
 if cmd_exists apt-get; then
   if cmd_exists pkmgr; then
     for pkg in libxml2-dev libmenu-cache-dev lxmenu-data libpango1.0-dev librsvg2-dev libcairo2-dev libxrandr-dev build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev; do
-      execute "pkmgr install -y $pkg" "Installing $pkg"
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
     done
   fi
-
 elif cmd_exists dnf; then
   if cmd_exists pkmgr; then
     for pkg in cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel; do
-      execute "pkmgr install -y $pkg" "Installing $pkg"
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
     done
   fi
-
 elif cmd_exists yum; then
   if cmd_exists pkmgr; then
     for pkg in cairo-devel xcb-util-devel libxcb-devel xcb-proto xcb-util-image-devel xcb-util-wm-devel; do
-      execute "pkmgr install -y $pkg" "Installing $pkg"
+      printf_color "$CYAN" "Installing $pkg"
+      pkmgr silent install $pkg &>/dev/null
+      [[ $? = 0 ]] && printf_color "$GREEN" "Installed $pkg" || printf_color "$RED" "Failed to installed $pkg"
     done
   fi
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-polybar_compile() {
-  if ! cmd_exists polybar; then
-    if [ -d "$HOME/.local/tmp/polybar" ]; then
-      cd "$HOME/.local/tmp/polybar"
-      git pull
-    else
-      git clone --recursive https://github.com/polybar/polybar "$HOME/.local/tmp/polybar"
-      cd "$HOME/.local/tmp/polybar"
-    fi
-    mkdir build
-    cd build
-    cmake ..
-    make -j$(nproc)
-    sudo make install
+# Main application
+if [[ -d "$BUILD_SRC_DIR" ]]; then
+  if ! builtin cd "$BUILD_SRC_DIR"; then
+    printf_color "$RED" "Failed to cd into $BUILD_SRC_DIR"
+    exit 1
   fi
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-jgmenu_compile() {
-  if ! cmd_exists jgmenu; then
-    if [ -d "$HOME/.local/tmp/jgmenu" ]; then
-      cd "$HOME/.local/tmp/jgmenu"
-      git pull
-    else
-      git clone --recursive https://github.com/johanmalm/jgmenu "$HOME/.local/tmp/jgmenu"
-      cd "$HOME/.local/tmp/jgmenu"
-    fi
-    ./configure --prefix=/usr/local --with-lx --with-pmenu
-    make
-    sudo make install
-  fi
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-execute \
-  "polybar_compile" \
-  "Compiling polybar"
-
-execute \
-  "jgmenu_compile" \
-  "Compiling jgmenu"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-if cmd_exists polybar; then
-  printf_green "\t\tpolybar has been installed\n"
-else
-  printf_red "\t\tpolybar has failed to build\n"
+  __run_git
+  __make_build
+  __check_log
+  exitCode=$?
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-if cmd_exists jgmenu; then
-  printf_green "\t\tjgmenu has been installed\n"
+# check
+if [[ "$exitCode" -eq 0 ]] && builtin type -P "$BUILD_NAME"; then
+  printf_color "$GREEN" "Successfully installed $BUILD_NAME"
+  exitCode=0
 else
-  printf_red "\t\tjgmenu has failed to build\n"
+  printf_color "$RED" "Failed to install $BUILD_NAME"
+  exitCode=1
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
-#/* vim: set expandtab ts=4 noai
